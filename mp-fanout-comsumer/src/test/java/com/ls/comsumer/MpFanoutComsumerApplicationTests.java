c79e7a34f5226278fa8b588cc91d6a091cf5c657
liusCoding <14786469221@163.com>
2019-09-12 09:38:01 +0800

    使用RabittMQ实现松耦合的设计

--- mp-fanout-comsumer/src/test/java/com/ls/comsumer/MpFanoutComsumerApplicationTests.java
+++ mp-fanout-comsumer/src/test/java/com/ls/comsumer/MpFanoutComsumerApplicationTests.java
@@ -5,12 +5,18 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Scanner;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MpFanoutComsumerApplicationTests {
 
     @Test
     public void contextLoads() {
+
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+
     }
 
 }
